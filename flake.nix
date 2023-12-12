92bd9c584adef2dfdac379bbd87010715ee5dbcf
Michael Lynch <git@mtlynch.io>
2023-12-11 23:42:06 -0500

    Revert "Switch to go modules"
    This reverts commit 7088d103b67e3b3a992508888882679d51f85a3e.

--- flake.nix
+++ flake.nix
@@ -6,26 +6,20 @@
 
     # 0.119.0 release
     hugo_dep.url = "github:NixOS/nixpkgs/75a52265bda7fd25e06e3a67dee3f0354e73243c";
-
-    # 1.21.4 release
-    go_dep.url = "github:NixOS/nixpkgs/a71323f68d4377d12c04a5410e214495ec598d4c";
   };
 
-  outputs = { self, nixpkgs, flake-utils, go_dep, hugo_dep }@inputs :
+  outputs = { self, nixpkgs, flake-utils, hugo_dep }@inputs :
     flake-utils.lib.eachDefaultSystem (system:
     let
       hugo_dep = inputs.hugo_dep.legacyPackages.${system};
-      go_dep = inputs.go_dep.legacyPackages.${system};
     in
     {
       devShells.default = hugo_dep.mkShell {
         packages = [
           hugo_dep.hugo
-          go_dep.go
         ];
 
         shellHook = ''
-          go version
           hugo version
         '';
       };
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/mtlynch/slide-decks
-
-go 1.21.3
-
-require github.com/dzello/reveal-hugo v0.0.0-20230621074321-b5fd252eab49 // indirect
